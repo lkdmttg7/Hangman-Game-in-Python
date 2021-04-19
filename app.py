ade8357a2ddafa724d7dc8a75533ca586a11d8ad
lkdmttg7 <inprovertmer07@gmail.com>
2021-04-19 23:38:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import random
+import time
+# Initial Steps to invite in the game:
+print("\nWelcome to Hangman Game\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Good Luck!")
+time.sleep(3)
+print("Are you Ready!!\n Let's play Hangman!")
+time.sleep(2)
