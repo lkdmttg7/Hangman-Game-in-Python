abea0336ce8fcfe9b9cbc93d436013eb805654f8
lkdmttg7 <inprovertmer07@gmail.com>
2021-04-20 23:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ print("Hello " + name + "! Good Luck!")
 time.sleep(3)
 print("Are you Ready!!\n Let's play Hangman!")
 time.sleep(2)
+def play_loop():
+    global play_game
+    play_game = input("Do You want to play again? y = yes, n = no \n")
+    while play_game not in ["y", "n","Y","N"]:
+        play_game = input("Do You want to play again? y = yes, n = no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Thanks For Playing! We expect you back again!")
+        exit()
