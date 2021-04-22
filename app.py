eb471d49fcb6817ad4a5da6b61b760b4c28e0aad
lkdmttg7 <inprovertmer07@gmail.com>
2021-04-22 22:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,17 @@ def play_loop():
     elif play_game == "n":
         print("Thanks For Playing! We expect you back again!")
         exit()
+
+# Initializing all the conditions required for the game:
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+        hangman()
